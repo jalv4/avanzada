1389364c314208ecbc697ca14afa611811360289
sariscarrion1904-creator <sariscarrion1904@gmail.com>
2026-03-13 11:38:59 -0500

    Define structures for equipment and users
    Added structures for Equipo, Usuario, and SesionUso.

--- avanzada.cpp
+++ avanzada.cpp
@@ -1,5 +1,38 @@
 #include <iostream>
 
+//Estructuras
+struct Equipo
+{
+    int codigo;
+    char nombre[100];
+    char laboratorio[50];
+    char tipo[50];
+    char estado[20];
+    float costo;
+    int semestreMin;
+    char descripcion[200];
+};
+
+struct Usuario
+{
+    long codigo;
+    char nombre[100];
+    char programa[80];
+    int semestre;
+};
+
+struct SesionUso
+{
+    int codigoSesion;
+    int codigoEquipo;
+    long codigoUsuario;
+    char fecha[20];
+    int duracion;
+    char observaciones[200];
+    float penalizacion;
+};
+
+//Funciones
 int main (){
   
 }
