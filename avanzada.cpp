d426adeecd7289dae6eb30417e6e45500c7c6573
laurahuertas198-maker <laurahuertas198@gmail.com>
2026-03-15 19:20:09 -0500

    Add function to consult operational status of equipment

--- avanzada.cpp
+++ avanzada.cpp
@@ -40,6 +40,38 @@ struct SesionUso
 
 
 //3.
+void consultarEstadoOpertaivoE(Equipo*equipos, int totalEquipos){
+    char nombreLab[100];
+    cout<<"Por favor ingrese el nombre del laboratorio: ";
+    cin.ignore();
+    cin.getline(nombreLab,100);
+    Equipo*ptr=equipos;
+    int operativos=0;
+    int mantenimiento=0;
+    int restriccion=0;
+    for(int i=0;i<totalEquipos;i++){
+        if(strcmp(ptr->laboratorio,nombreLab)==0){
+            if(strcmp(ptr->estado,"operativa")==0){
+                cout<<"Equipos operativos: "<<ptr->nombre<<endl;
+                    operativos++;
+            }
+            else if(strcmp(ptr->estado,"mantenimiento")==0){
+                cout<<"Equipos en mantenimiento: "<<ptr->nombre<<endl;
+                    mantenimiento++;
+            }
+        if(ptr->semestreMin>1){
+                cout<<"Equipos con restricciones por semestre: "<<ptr->nombre<<endl;
+                    restriccion++;
+            }
+        }
+        ptr++;
+    }
+    cout<<"Equipos operativos: "<<operativos<<endl;
+    cout<<"Equipos en mantenimiento: "<<mantenimiento<<endl;
+    cout<<"Equipos con restricciones por semestre: "<<restriccion<<endl;
+}
+
+
 
 
 //4.
