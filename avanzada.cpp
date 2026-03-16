a753345aaa0caab5322ba1da93d6398f7f5f223a
jalv4 <ccjalv4@gmail.com>
2026-03-16 15:23:25 -0500

    Update avanzada.cpp

--- avanzada.cpp
+++ avanzada.cpp
@@ -34,10 +34,177 @@ struct SesionUso
 
 //Funciones
 //1.
+    void cargaequipos(){
+        ifstream equipo(equipos.txt);
+        if(!equipo){
+            cout << "no se pudo abrir el archivo equipos\n";
+            exit;
+        }
+        int new n = 0;
+        char encapsulador[100];
 
+        while(equipo.getline(encapsulador, 100){
+            n++;
+        }
 
+        Equipo equipos[n];
+        int i = 0;
+        equipo.clear();
+        equipo.seekg(0);
+
+        while(equipos.getline(encapsulador, 100){
+               int p = 0;
+               int j = 0;
+
+                    while(encapsulador[p] != '*'){
+                        equipos[i].codigo[j] = encapsulador[p];
+                        p++;
+                        j++;
+                     }
+                        equipos[i].codigo[j] = '\0';
+                        p += 2;
+                        j = 0;
+
+        
+                    while(encapsulador[p] != '*'){
+                        equipos[i].nombre[j] = encapsulador[p];
+                        p++;
+                        j++;
+                    }
+                        equipos[i].nombre[j] = '\0';
+                        p += 2;
+                        j = 0;
+        
+                    while(encapsulador[p] != '*'){
+                        equipos[i].laboratorio[j] = encapsulador[p];
+                        p++;
+                        j++;
+                    }
+                        equipos[i].laboratorio[j] = '\0';
+
+                        p += 2;
+                        j = 0;
+
+                    while(encapsulador[p] != '*'){
+                        equipos[i].tipo[j] = encapsulador[p];
+                        p++;
+                        j++;
+                    }
+                        equipos[i].tipo[j] = '\0';
+                        p += 2;
+                        j = 0;
+
+                    while(encapsulador[p] != '*'){
+                        equipos[i].estado[j] = encapsulador[p];
+                        p++;
+                        j++;
+                    }
+                        equipos[i].estado[j] = '\0';
+                        p += 2;
+                        j = 0;
+
+    
+                    while(encapsulador[p] != '*'){
+                        equipos[i].costo[j] = encapsulador[p];
+                        p++;
+                        j++;
+                    }
+                        equipos[i].costo[j] = '\0';
+                        p += 2;
+                        j = 0;
+        
+                    while(encapsulador[p] != '*'){
+                        equipos[i].semestre[j] = encapsulador[p];
+                        p++;
+                        j++;
+                    }
+                        equipos[i].semestre[j] = '\0';
+
+                        p += 2;
+                        j = 0;
+
+                    while(encapsulador[p] != '\0'){
+                        equipos[i].descripcion[j] = encapsulador[p];
+                        p++;
+                        j++;
+                    }
+                        equipos[i].descripcion[j] = '\0';
+
+                i++;
+            }
+
+    archivo.close();
+}
+        
 //2.
+void cargausuarios(){
 
+    char encapsulador[100];
+    int i = 0;
+
+    ifstream usuario("usuarios.txt");
+
+    if(!usuario){
+        cout << "no se pudo abrir el archivo usuarios\n";
+        return;
+    }
+
+        int new m = 0;
+        char encapsulador[100];
+
+        while(usuario.getline(encapsulador, 100){
+            m++;
+        }
+
+        Equipo usuarios[n];
+        int i = 0;
+        usuario.clear();
+        usuario.seekg(0);
+
+    while(usuario.getline(encapsulador,100)){
+
+        int p = 0;
+        int j = 0;
+
+        while(encapsulador[p] != '*'){
+            usuarios[i].codigo[j] = encapsulador[p];
+            p++;
+            j++;
+        }
+            usuarios[i].codigo[j] = '\0';
+            p += 2;
+            j = 0;
+
+        while(encapsulador[p] != '*'){
+            usuarios[i].nombre[j] = encapsulador[p];
+            p++;
+            j++;
+        }
+            usuarios[i].nombre[j] = '\0';
+            p += 2;
+            j = 0;
+        
+        while(encapsulador[p] != '*'){
+            usuarios[i].programa[j] = encapsulador[p];
+            p++;
+            j++;
+        }
+            usuarios[i].programa[j] = '\0';
+            p += 2;
+            j = 0;
+        
+        while(encapsulador[p] != '\0'){
+            usuarios[i].semestre[j] = encapsulador[p];
+            p++;
+            j++;
+        }
+            usuarios[i].semestre[j] = '\0';
+
+        i++;
+    }
+
+    archivo.close();
+}
 
 //3.
 void consultarEstadoOpertaivoE(Equipo*equipos, int totalEquipos){
